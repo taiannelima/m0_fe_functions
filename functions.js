c1713f3e1655200a3b1cb65a4f7991ec76528e73
Taianne Lima <rtaianne@gmail.com>
2023-11-12 17:37:19 -0800

    functions done

--- functions.js
+++ functions.js
@@ -5,28 +5,57 @@
 
 // 1: Write a function named greeting that returns a string with a general greeting. 
 
-
-// 2: Write a function named customGreeting that returns a greeting WITH a specific name.
-
-
-// 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
-
-
-// 4: Write a function named square that takes in one number, and returns the square of that number.
-// BONUS: Print a sentence that interpolates the return value of your square function.
-
-
-// 5: Write a function named checkStock that satisfies the following interaction pattern:
-// Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
-
-checkStock(4, "Coffee");
-// => "Coffee is stocked"
-
-checkStock(3, "Tortillas");
-// => "Tortillas - running LOW"
-
-checkStock(0, "Cheese");
-// => "Cheese - OUT of stock!"
-
-checkStock(1, "Salsa");
-// => "Salsa - running LOW"
+function greeting() {
+    return "Hello, welcome!"; 
+       }
+  var greeting = greeting();
+  console.log(greeting);
+  
+  
+  // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
+  
+  function customGreeting(name) {
+    return "Hello, dear Visitor ";
+      }
+  var JohnGreeting = customGreeting("John"); 
+  console.log(JohnGreeting);
+  
+  
+  
+  // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+  
+  
+  function greetPerson(firstName, middleName, lastName) {
+    return "Hello, dear " + "Johnn" +  " Gosling " + "Junior";
+  
+  }
+  
+  var fullGreeting = greetPerson();
+  console.log(fullGreeting);
+  
+  
+  // 4: Write a function named square that takes in one number, and returns the square of that number.
+  // BONUS: Print a sentence that interpolates the return value of your square function.
+  
+  function square (number) {
+    return number * number;
+  }
+  console.log(square(7));  
+  
+  
+  
+  
+  function checkStock(quantity, item) {
+    if (quantity === 0) {
+      console.log(`${item} - OUT of stock!`);
+    } else if (quantity <= 3) {
+      console.log(`${item} - running LOW`);
+    } else {
+      console.log(`${item} is stocked`);
+    }
+  }
+  checkStock(4, "Coffee");
+  checkStock(3, "Tortillas");
+  checkStock(0, "Cheese");
+  checkStock(1, "Salsa");
+  
